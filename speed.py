288ea5eb81f62a4c41b121d0b35c4f9ed63fcd4d
Jordan <jordanbourcier@protonmail.com>
2022-05-09 08:34:34 -0400

    2nd commit

--- speed.py
+++ speed.py
@@ -3,10 +3,11 @@ import subprocess
 import re
 
 def get_speed():
+    print('>> Checking download speed....')
     output = subprocess.run(['speedtest'], capture_output=True).stdout.decode()
     re_exp = 'Download:\s(.*)'
     internet_speed = re.findall(re_exp, output)
-    print(internet_speed)
+    print(internet_speed[0])
     
     
 if __name__ == '__main__':
