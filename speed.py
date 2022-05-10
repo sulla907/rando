2d96cb86472aa411d64337256aa88f910bc9b244
Jordan <jordanbourcier@protonmail.com>
2022-05-10 14:39:24 -0400

    Fixed show data

--- speed.py
+++ speed.py
@@ -51,7 +51,7 @@ def show_data():
     db = sqlite3.connect('internet_speed.db')
     query = " SELECT * FROM download_speed"
     sql_data = pandas.read_sql_query(query, db)
-    print(sql_data.head())
+    print(sql_data)
     
 if __name__ == '__main__':
     speed = get_speed()
