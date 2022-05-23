c5238c5ec87e291a17c1edc3ba03a95cd5106a2b
Jordan <jordanbourcier@protonmail.com>
2022-05-23 10:11:09 -0400

    removed username, password, and IP

--- remote_db.py
+++ remote_db.py
@@ -1,7 +1,7 @@
 import psycopg2
 
 print('>> Attempting to connect to database....')
-con = psycopg2.connect(database='test', user='warPig', password='test', host='3.144.128.71', port='5432')
+con = psycopg2.connect(database='test', user='', password='', host='', port='5432')
 print('>> Connected to database')
 c = con.cursor()
 make_table = '''CREATE TABLE download_speed (timestamp text, speed text)'''
